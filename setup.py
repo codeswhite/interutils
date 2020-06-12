4f7c590d5abcc2a54fbacadc1c62317229e7c517
codeswhite <max3227@gmail.com>
2020-06-12 13:16:39 +0300

    Fixed typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="interutils",
     version="0.8.0",
-    author="Max Grinberg",
+    author="Max G",
     author_email="max3227@gmail.com",
     description="A collection of handy utilities and snippets for creating interactive programs.",
     long_description=long_description,
