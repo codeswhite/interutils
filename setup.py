fd705637bc1c1158528952fb24f9e1b1449ffdbe
codeswhite <max3227@gmail.com>
2020-06-12 13:40:40 +0300

    Restructurized the project

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="interutils",
-    version="0.8.0",
+    version="0.8.2",
     author="Max G",
     author_email="max3227@gmail.com",
     description="A collection of handy utilities and snippets for creating interactive programs.",
