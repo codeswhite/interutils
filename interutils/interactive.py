4af1641e71277d6da8666053bf6f4775d3dd36ca
maximus <max3227@gmail.com>
2020-06-14 01:02:47 +0300

    Removed obsolete function

--- interutils/interactive.py
+++ interutils/interactive.py
@@ -96,33 +96,6 @@ def banner(txt: str, style: str = 'slant') -> str:
     return f.renderText(text=txt)
 
 
-def generic_menu_loop(directory: str, menu: dict) -> None:
-    while 1:
-        inp = input(colored(f'.{directory}->', 'red', attrs=['bold']))
-        if not inp:
-            break
-        if 'help' in inp:
-            for c in menu:
-                v = menu[c]
-                desc = v[1] if type(
-                    v[1]) is str else f'Enter {v[0].capitalize()} menu'
-                print(f'  {cyan(c)} -> {colored(desc, "yellow")}')
-            continue
-
-        pts = inp.split(' ')
-        if pts[0] not in menu:
-            pr('No such command! try "help".', '!')
-            continue
-
-        command = menu.get(pts[0])
-        func = command[0]
-        if callable(func):
-            func(tuple([i for i in pts[1:] if i]))
-        else:
-            generic_menu_loop(f'{directory}.{func}', command[1])
-        print()
-
-
 def get_date() -> str:
     """
     :return: today's date (e.g. "28.11.2017" ;P)
