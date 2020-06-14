e688f56499e7703dcc4274a3aed800b8c36dc090
Max G <max3227@gmail.com>
2020-06-14 06:06:37 +0300

    Another HotFix

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,7 @@
 import unittest
 import pathlib
 
-from interutils.interactive import clear, pr, ask, banner, choose, pause, cyan, generic_menu_loop, get_date
-from interutils.net import get_net, is_mac, ping
-from interutils.fs import choose_file, count_lines, file_volume, human_bytes, is_image, is_package
-
+from interutils import *
 
 class TestFS(unittest.TestCase):
     def setUp(self):
